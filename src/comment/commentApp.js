22769b52082f88a3278ffacb4f807682bd385af6
YIDA <yida1011@gmail.com>
2018-11-21 18:03:22 +0800

    提交commentApp

---
+++ src/comment/commentApp.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import CommentInput from './commentInput'
+import CommentList from './commentList'
+import './index.css'
+class CommentApp extends Component {
+    constructor() {
+        super();
+        this.state ={
+            comments: [
+                { username: 'Jerry', content: 'Hello' },
+                { username: 'Tomy', content: 'World' },
+                { username: 'Lucy', content: 'Good2' }
+            ]
+        }
+    }
+    handleSubmitComment(comment) {
+        if(!comment) {return;}
+        if(!comment.username) {
+            return alert('请输入用户名');
+        }
+        if(!comment.content) {
+            return alert('请输入评论内容');
+        }
+        this.state.comments.push(comment);
+        this.setState({
+            comments: this.state.comments
+        })
+    }
+
+    render() {
+        return (
+            <div className="wrapper">
+                <CommentInput onSubmit={this.handleSubmitComment.bind(this)} />
+                <CommentList comments={this.state.comments} />
+            </div>
+        )
+    }
+}
+
+export default CommentApp
